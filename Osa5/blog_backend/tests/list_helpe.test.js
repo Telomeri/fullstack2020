03ee1f67c5147b7d4698f2b1aa889e10d5dda2a6
Telomeri <43409911+Telomeri@users.noreply.github.com>
2020-08-28 13:23:46 +0300

    Add files via upload

---
+++ Osa5/blog_backend/tests/list_helpe.test.js
@@ -0,0 +1,97 @@
+const listHelper = require('../utils/list_helper')
+// eslint-disable-next-line quotes
+const blogs = [ { _id: "5a422a851b54a676234d17f7", title: "React patterns", author: "Michael Chan", url: "https://reactpatterns.com/", likes: 7, __v: 0 }, { _id: "5a422aa71b54a676234d17f8", title: "Go To Statement Considered Harmful", author: "Edsger W. Dijkstra", url: "http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html", likes: 5, __v: 0 }, { _id: "5a422b3a1b54a676234d17f9", title: "Canonical string reduction", author: "Edsger W. Dijkstra", url: "http://www.cs.utexas.edu/~EWD/transcriptions/EWD08xx/EWD808.html", likes: 12, __v: 0 }, { _id: "5a422b891b54a676234d17fa", title: "First class tests", author: "Robert C. Martin", url: "http://blog.cleancoder.com/uncle-bob/2017/05/05/TestDefinitions.htmll", likes: 10, __v: 0 }, { _id: "5a422ba71b54a676234d17fb", title: "TDD harms architecture", author: "Robert C. Martin", url: "http://blog.cleancoder.com/uncle-bob/2017/03/03/TDD-Harms-Architecture.html", likes: 0, __v: 0 }, { _id: "5a422bc61b54a676234d17fc", title: "Type wars", author: "Robert C. Martin", url: "http://blog.cleancoder.com/uncle-bob/2016/05/01/TypeWars.html", likes: 2, __v: 0 }]
+const emptylist = []
+const oneblog = [
+  {
+    _id: '5a422aa71b54a676234d17f8',
+    title: 'Go To Statement Considered Harmful',
+    author: 'Edsger W. Dijkstra',
+    url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
+    likes: 5,
+    __v: 0
+  }]
+
+describe('total likes', () => {
+
+  test('of empty list is zero', async () => {
+    const result = listHelper.total_likes(emptylist)
+    expect(result).toBe(0)
+  })
+
+  test('when list has only one blog equals the likes of that', async () => {
+    const result = listHelper.total_likes(oneblog)
+    expect(result).toBe(5)
+  })
+
+  test('of a bigger list is calculated right', async () => {
+    const result = listHelper.total_likes(blogs)
+    expect(result).toBe(36)
+  })
+})
+
+describe('favoriteBlog', () => {
+
+  test('of a empty list', async () => {
+    const result = listHelper.favoriteBlog(emptylist)
+    expect(result).toEqual({})
+  })
+
+  test('of a single blog', async () => {
+    const result = listHelper.favoriteBlog(oneblog)
+    expect(result).toEqual({ title:'Go To Statement Considered Harmful', author: 'Edsger W. Dijkstra', likes: 5 })
+  })
+
+  test('of a list of blogs', async () => {
+    const result = listHelper.favoriteBlog(blogs)
+    expect(result).toEqual({ title:'Canonical string reduction', author: 'Edsger W. Dijkstra', likes: 12 })
+  })
+})
+
+//4.6
+describe('mostBlogs', () => {
+
+  test('of a empty list',  async () => {
+    const result = listHelper.mostBlogs(emptylist)
+    expect(result).toEqual(null)
+  })
+
+  test('of a single blog', async () => {
+    const result = listHelper.mostBlogs(oneblog)
+    expect(result).toEqual({ author: 'Edsger W. Dijkstra', blogs: 1 })
+  })
+
+  test('of a list of blogs', async () => {
+    const result = listHelper.mostBlogs(blogs)
+    expect(result).toEqual({
+      author:'Robert C. Martin',
+      blogs: 3
+    })
+  })
+})
+
+//4.7
+describe('mostLikes', () => {
+
+  test('of a empty list',  async () => {
+    const result = listHelper.mostLikes(emptylist)
+    expect(result).toEqual(null)
+  })
+
+  test('of a single blog', async () => {
+    const result = listHelper.mostLikes(oneblog)
+    expect(result).toEqual({ author: 'Edsger W. Dijkstra', likes: 5 })
+  })
+
+  test('of a list of blogs', async () => {
+    const result = listHelper.mostLikes(blogs)
+    expect(result).toEqual({
+      author: 'Edsger W. Dijkstra',
+      likes: 17
+    })
+  })
+})
+
+//I get weird errors from blog_api.test.js even when running these tests,
+//but they dissapear if I comment that code out. probably because those tests
+//no longer work
